c3d1b45b8d039bcb61bebc83463100b8f7fbf58c
markhmwong <markhmwong@gmail.com>
2019-06-13 15:37:37 +1000

    Initial Commit

---
+++ ReadyPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadyPlayer
+//
+//  Created by Mark Wong on 13/6/19.
+//  Copyright © 2019 Mark Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
